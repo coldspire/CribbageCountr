687ca8349fe3c469dd3689f6420cf3845fb9f473
coldspire <owenra+git@gmail.com>
2014-05-04 11:44:52 -0400

    Added deck tests

---
+++ Tests/DeckTests.cs
@@ -0,0 +1,31 @@
+﻿using CribbageCountr;
+using Xunit;
+
+namespace Tests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void DeckDefaultHas52Cards()
+        {
+            Deck deck = new Deck();
+            Assert.Equal(52, deck.NumberOfCards);
+        }
+
+        [Fact]
+        public void DeckWithJokersHas54Cards()
+        {
+            Deck deck = new Deck(true, false);
+            Assert.Equal(54, deck.NumberOfCards);
+        }
+
+        [Fact]
+        public void DeckWithOneDupeHas53Cards()
+        {
+            Deck deck = new Deck(true, true);
+            deck.AddCard(new Card(Suit.Club, Rank.Ace));
+            
+            Assert.Equal(53, deck.NumberOfCards);
+        }
+    }
+}
