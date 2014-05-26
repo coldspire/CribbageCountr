c381fd2fbfb9c46225c016bdc3c8004e23a70b59
coldspire <owenra+git@gmail.com>
2014-05-26 10:51:33 -0400

    Deck draw tests moved to own file, DeckDrawTests.cs

--- Tests/DeckTests.cs
+++ Tests/DeckTests.cs
@@ -1,6 +1,4 @@
-﻿
-using CribbageCountr;
-using System.Collections.Generic;
+﻿using CribbageCountr;
 using System.Linq;
 using Xunit;
 
@@ -71,74 +69,6 @@ namespace Tests
             Deck deck = new Deck();
             Assert.Throws<System.ArgumentOutOfRangeException>(() => deck[-1]);
             Assert.Throws<System.ArgumentOutOfRangeException>(() => deck[deck.NumberOfCards+1]);
-        }
-        #endregion
-
-        #region Draw tests
-        [Fact]
-        public void DeckDrawOf5Returns5Cards()
-        {
-            const int NumCardsToRetrieve = 5;
-
-            Deck deck = new Deck();
-            Card[] cards = deck.Draw(NumCardsToRetrieve).ToArray();
-
-            Assert.True(cards.Length == NumCardsToRetrieve);
-        }
-
-        [Fact]
-        public void DeckDrawNegativeNumThrowsExcept()
-        {
-            const int NegNumCardsToRetrieve = -1;
-            Deck deck = new Deck();
-
-            Assert.Throws<System.ArgumentOutOfRangeException>(() => deck.Draw(NegNumCardsToRetrieve));
-        }
-
-        [Fact]
-        public void DeckDrawMoreThanAllGetsAll()
-        {
-            Deck deck = new Deck();
-
-            int numTotalCards = deck.NumberOfCards;
-            int numMoreThanTotal = deck.NumberOfCards + 1;
-
-            Card[] cards = deck.Draw(numMoreThanTotal).ToArray();
-            Assert.True(cards.Count() == numTotalCards);
-        }
-
-        [Fact]
-        public void DeckDrawZeroReturnsEmptyArray()
-        {
-            Deck deck = new Deck();
-            Card[] cards = deck.Draw(0).ToArray();
-
-            Assert.True(cards != null);
-            Assert.True(cards.Length == 0);
-        }
-
-        [Fact]
-        public void DeckDrawReturnsOnlyUnplayed()
-        {
-            const int CardsToLeaveNotPlayed = 3;
-
-            Deck deck = new Deck();
-            deck.Draw(deck.NumberOfCards - CardsToLeaveNotPlayed); // Draw most cards, i.e. mark most cards as Played
-
-            Card[] cards = deck.Draw(CardsToLeaveNotPlayed+1).ToArray(); // Intentionally draw more cards than are not Played
-            Assert.True(cards.Length == CardsToLeaveNotPlayed);
-        }
-
-        [Fact]
-        public void DeckDrawDrawingAllMarksAllAsPlayed()
-        {
-            Deck deck = new Deck();
-            deck.Draw(deck.NumberOfCards-1);
-            Assert.True(!deck.AllPlayed); // Still have one unplayed card
-
-            deck.Draw(1); // Draw the final unplayed card
-            Assert.True(deck.Draw(1).Count() == 0);
-            Assert.True(deck.AllPlayed);
         }
         #endregion
 
