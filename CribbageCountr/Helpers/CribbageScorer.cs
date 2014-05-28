6a78b8498e1690b7d393b4b4cc21bd7819262378
coldspire <owenra+git@gmail.com>
2014-05-28 08:48:31 -0400

    Added CribbageScorer.cs and stub

---
+++ CribbageCountr/Helpers/CribbageScorer.cs
@@ -0,0 +1,35 @@
+﻿using CribbageCountr;
+
+namespace CribbageCountr
+{
+    public static class CribbageScorer
+    {
+        // TODO: Sum of 15 ()
+
+        // TODO: 2-of-a-kind, pair (2)
+        // TODO: 3-of-a-kind (6, three pairs)
+        // TODO: 4-of-a-kind (12, six pairs)
+
+        // TODO: Run of 3 (3)
+        // TODO: Run of 4 (4)
+        // TODO: Run of 5 (5)
+
+        // TODO: Run of 3, plus a pair  (8: two run-of-threes, plus a pair)
+        // TODO: Run of 4, plus a pair (10: two run-of-fours, plus a pair)
+        // TODO: Triple run (15: three pairs, plus a run-of-three)
+
+        // TODO: Flush of 4 (4)
+        // TODO: Flush of 5 (5)
+
+        // TODO: His Nobs (1: Jack is same suit as cut card)
+
+        public static int ScoreHand(Card[] cards)
+        {
+            int pointsTotal = 0;
+
+
+
+            return (pointsTotal);
+        }
+    }
+}
