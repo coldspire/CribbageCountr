cea0731ef2ab7c9d27a5227e6e94282e0fd14d5a
coldspire <owenra+git@gmail.com>
2014-06-15 21:37:36 -0400

    Start of GetSets() and tests

--- CribbageCountr/Helpers/CribbageScorer.cs
+++ CribbageCountr/Helpers/CribbageScorer.cs
@@ -1,10 +1,12 @@
 ﻿using CribbageCountr;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace CribbageCountr
 {
     public static class CribbageScorer
     {
-        // TODO: Sum of 15 ()
+        // TODO: Sum of 15 (2)
 
         // TODO: 2-of-a-kind, pair (2)
         // TODO: 3-of-a-kind (6, three pairs)
@@ -22,8 +24,37 @@ namespace CribbageCountr
         // TODO: Flush of 5 (5)
 
         // TODO: His Nobs (1: Jack is same suit as cut card)
+        
+        public static IEnumerable<List<Card>> GetSets(int setWidth, Card[] cards)
+        {
+            if (setWidth <= 0)
+            {
+                throw new System.ArgumentOutOfRangeException("setWidth cannot be zero or negative.");
+            }
+            
+            List<List<Card>> sets = new List<List<Card>>();
 
-        public static int ScoreHand(Card[] cards)
+            if (setWidth == 1)
+            {
+                // You get back a bunch of single-card sets.
+                foreach (Card card in cards)
+                {
+                    List<Card> set = new List<Card>() { card };
+                    sets.Add(set);
+                }
+                return (sets.AsEnumerable());
+            }
+
+            return (sets.AsEnumerable());
+        }
+
+        /// <summary>
+        /// Returns the score of a single cribbage hand.
+        /// </summary>
+        /// <param name="hand">All cards in the hand to be scored.</param>
+        /// <param name="cutCard">A card in the hand that is the cut card.</param>
+        /// <returns>Number of scored points.</returns>
+        public static int ScoreHand(Card[] hand, Card cutCard)
         {
             int pointsTotal = 0;
 
---
+++ Tests/ScorerTests.cs
@@ -0,0 +1,31 @@
+﻿using CribbageCountr;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Tests
+{
+    public class ScorerTests
+    {
+        // Test that GetSets() asserts when width <= 0 is passed in
+
+        [Fact]
+        public void WidthOfOneReturnsSingleCardSets()
+        {
+            Card[] cards = 
+            { 
+                new Card(Suit.Club, Rank.Ace),
+                new Card(Suit.Club, Rank.Two),
+                new Card(Suit.Club, Rank.Three),
+                new Card(Suit.Club, Rank.Four)
+            };
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(1, cards);
+
+            Assert.True(sets.Count() == 4);
+            foreach (List<Card> set in sets)
+            {
+                Assert.True(set.Count == 1);
+            }
+        }
+    }
+}
