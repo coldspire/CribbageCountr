021f917383fb88b8080b684f0b497f8f53f4674b
coldspire <owenra+git@gmail.com>
2014-06-18 21:26:46 -0400

    Cribbage Scorer: Moved cards[] definitions to class def for common use

--- Tests/ScorerTests.cs
+++ Tests/ScorerTests.cs
@@ -7,34 +7,39 @@ namespace Tests
 {
     public class ScorerTests
     {
+        private Card[] cards1 = 
+        { 
+            new Card(Suit.Club, Rank.Ace) 
+        };
+
+        Card[] cards4 = 
+        { 
+            new Card(Suit.Club, Rank.Ace),
+            new Card(Suit.Club, Rank.Two),
+            new Card(Suit.Club, Rank.Three),
+            new Card(Suit.Club, Rank.Four)
+        };
+
         [Fact]
         public void GetSetsWidthOfZeroOrNegAsserts()
         {
-            Card[] cards = { new Card(Suit.Club, Rank.Ace) };
+            Assert.Throws(typeof(System.ArgumentOutOfRangeException),
+                          () => { CribbageScorer.GetSets(0, cards1); });
 
             Assert.Throws(typeof(System.ArgumentOutOfRangeException),
-                          () => { CribbageScorer.GetSets(0, cards); });
-
-            Assert.Throws(typeof(System.ArgumentOutOfRangeException),
-                          () => { CribbageScorer.GetSets(-1, cards); });
+                          () => { CribbageScorer.GetSets(-1, cards1); });
         }
 
         [Fact]
         public void GetSets_Width1_Cards4_Returns4Sets()
         {
-            Card[] cards = 
-            { 
-                new Card(Suit.Club, Rank.Ace),
-                new Card(Suit.Club, Rank.Two),
-                new Card(Suit.Club, Rank.Three),
-                new Card(Suit.Club, Rank.Four)
-            };
-            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(1, cards);
+            const int Width = 1;
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards4);
 
             Assert.True(sets.Count() == 4);
             foreach (List<Card> set in sets)
             {
-                Assert.True(set.Count == 1);
+                Assert.True(set.Count == Width);
             }
         }
 
@@ -42,14 +47,7 @@ namespace Tests
         public void GetSets_Width2_Cards4_Returns6Sets()
         {
             const int Width = 2;
-            Card[] cards = 
-            { 
-                new Card(Suit.Club, Rank.Ace),
-                new Card(Suit.Club, Rank.Two),
-                new Card(Suit.Club, Rank.Three),
-                new Card(Suit.Club, Rank.Four)
-            };
-            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards);
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards4);
 
             Assert.True(sets.Count() == 6);
             foreach (List<Card> set in sets)
