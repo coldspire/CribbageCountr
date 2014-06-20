9ab728d2199a29515353e6d4f85c2ce953debda4
coldspire <owenra+git@gmail.com>
2014-06-19 21:00:17 -0400

    Scorer: Moved GetSets tests to own source file

---
+++ Tests/ScorerGetSetsTests.cs
@@ -0,0 +1,73 @@
+﻿using CribbageCountr;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+    public class ScorerGetSetsTests
+    {
+
+        private Card[] cards1 = 
+        { 
+            new Card(Suit.Club, Rank.Ace) 
+        };
+
+        private Card[] cards4 = 
+        { 
+            new Card(Suit.Club, Rank.Ace),
+            new Card(Suit.Club, Rank.Two),
+            new Card(Suit.Club, Rank.Three),
+            new Card(Suit.Club, Rank.Four)
+        };
+
+        [Fact]
+        public void GetSetsWidthOfZeroOrNegAsserts()
+        {
+            Assert.Throws(typeof(System.ArgumentOutOfRangeException),
+                          () => { CribbageScorer.GetSets(0, cards1); });
+
+            Assert.Throws(typeof(System.ArgumentOutOfRangeException),
+                          () => { CribbageScorer.GetSets(-1, cards1); });
+        }
+
+        [Fact]
+        public void GetSets_Width1_Cards4_Returns4Sets()
+        {
+            const int Width = 1;
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards4);
+
+            Assert.True(sets.Count() == 4);
+            foreach (List<Card> set in sets)
+            {
+                Assert.True(set.Count == Width);
+            }
+        }
+
+        [Fact]
+        public void GetSets_Width2_Cards4_Returns6Sets()
+        {
+            const int Width = 2;
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards4);
+
+            Assert.True(sets.Count() == 6);
+            foreach (List<Card> set in sets)
+            {
+                Assert.True(set.Count() == Width);
+            }
+        }
+
+        [Fact]
+        public void GetSets_Width4_Cards4_Returns1Set()
+        {
+            const int Width = 4;
+            IEnumerable<List<Card>> sets = CribbageScorer.GetSets(Width, cards4);
+
+            Assert.Equal(1, sets.Count());
+            if (sets.Count() == 1)
+            {
+                Assert.Equal(cards4.Count(), sets.Single<List<Card>>().Count()); // Should contain all cards
+            }
+        }
+    }
+}
